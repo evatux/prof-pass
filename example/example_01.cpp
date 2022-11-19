d3e58356b915581cce21498b035dc3a81e52258c
evatux <evatux@gmail.com>
2022-11-18 20:44:29 -0800

    example: complexify

--- example/example_01.cpp
+++ example/example_01.cpp
@@ -1,4 +1,23 @@
 #include <stdio.h>
+#include <unistd.h>
+
+#include <string>
+#include <map>
+
+template <typename T>
+struct my_class_t {
+    std::map<T, int> data_;
+    void add(T t) {
+        auto counter = [&]() {
+           static int count{};
+           ++count;
+           unsigned ms = (unsigned)count * 50 * 1000;
+           usleep(ms);
+           return count;
+        };
+        data_[t] = counter();
+    }
+};
 
 static constexpr int value = 10;
 
@@ -14,5 +33,8 @@ int foo() {
 int main() {
     int x = foo();
     printf("foo():%d expected:%d\n", x, value);
+    my_class_t<std::string> cache;
+    cache.add("a");
+    cache.add("b");
     return 0;
 }
